836024ee4f90f59f9463ba6514a9171e9dd75eb5
Niv Govrin <niv.govrin@gmail.com>
2023-08-05 12:02:45 +0300

    delete unused files

--- app/i18n/formatted-date.ts
+++
@@ -1,8 +0,0 @@
-import { formatRelative, differenceInMinutes } from "date-fns"
-import { dateFnsLocalization, translate } from "."
-
-export function useFormattedDate(date: Date) {
-  const now = new Date()
-  if (differenceInMinutes(date, now) < 2) return translate("plan.now")
-  return formatRelative(date, now, { locale: dateFnsLocalization })
-}
